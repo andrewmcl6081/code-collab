f9a143c090da5254eb9ceefc71496d69a8895494
DESKTOP-UCETDSH\andre <andrewmcl6081@gmail.com>
2025-03-15 09:27:18 -0500

    Worked on implementing auth0

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,15 @@
 import CodeEditor from "../components/CodeEditor";
+import { auth0 } from "@/lib/auth0";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    return (
+      <a href="/auth/login">Login</a>
+    );
+  }
+  
   return (
     <>
       <CodeEditor />
