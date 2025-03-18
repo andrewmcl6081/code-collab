1ecb1ff6ddee1c7d485ea63efa22daf08805eda9
DESKTOP-UCETDSH\andre <andrewmcl6081@gmail.com>
2025-03-18 16:05:17 -0500

    Prisma schema changes

---
+++ client/prisma/migrations/20250318203617_add_invite_code/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[inviteCode]` on the table `Room` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `createdBy` to the `Room` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `inviteCode` to the `Room` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Room" ADD COLUMN     "createdBy" TEXT NOT NULL,
+ADD COLUMN     "inviteCode" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Room_inviteCode_key" ON "Room"("inviteCode");
